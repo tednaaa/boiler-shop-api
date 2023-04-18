0d990b8e01d3de496f0195132141d3fd81d8e36d
And Grigoryan <tednaaa@gmail.com>
2023-04-18 21:13:26 +0400

    add psql & sequelize config

---
+++ src/config/sql.config.ts
@@ -0,0 +1,13 @@
+import { registerAs } from '@nestjs/config';
+
+export const sqlConfig = registerAs('database', () => ({
+  dialect: process.env.SQL_DIALECT,
+  logging: Boolean(process.env.SQL_LOGGING),
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  autoLoadEntities: true,
+  synchronize: true,
+}));
